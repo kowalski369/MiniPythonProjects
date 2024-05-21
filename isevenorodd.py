5cca5c175a32fc7826f4220a2e774bd37ae42b9f
Kowalski <kowalskiunit8200@gmail.com>
2024-05-21 21:30:35 +0530

    just made isevenorodd.py and completed in initial commit

---
+++ isevenorodd.py
@@ -0,0 +1,7 @@
+inp = int(input("Enter the NUmber: "))
+if inp%2 == 0 :
+    print("The number is even.")
+elif inp==0:
+    print("0 is neither odd or even.")
+else :
+    print("The number is odd.")
